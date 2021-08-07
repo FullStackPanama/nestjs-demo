e438a6e0b9146f9126af357eee0476c9df588bba
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-08-07 23:31:10 +0000

    Instalar dependencia class-validator, class-transformer y configurar DTOs para /posts

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  author_id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+}
