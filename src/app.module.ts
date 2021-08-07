3b17764566a13d66f2a285da01e2bc9f29d26de7
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-08-07 23:03:02 +0000

    Usar generador de CRUD para /posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TareasModule } from './tareas/tareas.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [TareasModule],
+  imports: [TareasModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
