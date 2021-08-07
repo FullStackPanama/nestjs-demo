947ccabe5bfa184b645ae89093ff31f2663c0453
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-08-07 23:32:03 +0000

    Crear rutas para /posts en Postman y modificar valores devueltos por métodos de PostService

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -5,22 +5,37 @@ import { UpdatePostDto } from './dto/update-post.dto';
 @Injectable()
 export class PostsService {
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return {
+      msg: 'This action adds a new post',
+      data: [createPostDto],
+    };
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return {
+      msg: `This action returns all posts`,
+      data: [],
+    };
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} post`;
+    return {
+      msg: `This action returns a #${id} post`,
+      data: [],
+    };
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+    return {
+      msg: `This action updates a #${id} post`,
+      data: [updatePostDto],
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    return {
+      msg: `This action removes a #${id} post`,
+      data: [],
+    };
   }
 }
