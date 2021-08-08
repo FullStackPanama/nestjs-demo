9c90a480238d9743077c3a8953d2ed79c8f1b72f
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-08-08 00:45:57 +0000

    Configurar ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TareasModule } from './tareas/tareas.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [TareasModule, PostsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database',
+      port: 3306,
+      username: 'demo',
+      password: 'fullstackpanama2021',
+      database: 'demo',
+      entities: [join(__dirname, '../**/**/*entity{.ts,.js}')],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TareasModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
