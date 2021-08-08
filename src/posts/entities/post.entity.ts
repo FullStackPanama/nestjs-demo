03e241eec3444d2ba37af1f8b577f388f6dca3fb
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2021-08-08 00:45:41 +0000

    Configurar entidad Post

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,21 @@
-export class Post {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text', nullable: false })
+  author_id!: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  title!: string;
+
+  @Column({ type: 'varchar', length: 500, nullable: false })
+  description!: string;
+
+  @Column({ type: 'text', nullable: false })
+  content!: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  date!: Date;
+}
